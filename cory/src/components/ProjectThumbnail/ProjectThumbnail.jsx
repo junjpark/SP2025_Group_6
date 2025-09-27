7d82b00078df0c0c65b0186548a997e9e18db04c
Abby Shen <143675084+aeshen27@users.noreply.github.com>
2025-09-27 11:51:09 -0500

    Add create new tile

--- cory/src/components/ProjectThumbnail/ProjectThumbnail.jsx
+++ cory/src/components/ProjectThumbnail/ProjectThumbnail.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./ProjectThumbnail.css";
 
-export default function ProjectThumbnail({ id, title, imageUrl}) {
+export default function ProjectThumbnail({ id, title, imageUrl, isCreate}) {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const handleClick = () => {
+        if (isCreate) {
+            setIsModalOpen(true);
+        } else {
+            // navigate to project view
+        }
+    }
     const thumbnail = (
-        <div className="project-thumbnail" key={id}>
-            <img src={imageUrl} alt={title} className="thumbnail-image" />
+        <div className="project-thumbnail" key={id} onClick={handleClick}>
+            {isCreate ? (
+                <div className="create-thumbnail">
+                    <span className="plus-icon">+</span>
+                    
+                </div>
+                ) : (
+                <>
+                    <img src={imageUrl} alt={title} className="thumbnail-image" />
+                </>
+                )}
             <h3 className="project-title">{title}</h3>
         </div>
     );
