f1c603349ea44bf32d48130b34dbfbd5da889f70
Abby Shen <143675084+aeshen27@users.noreply.github.com>
2025-09-30 18:31:33 -0500

    LINTTTT and accessibility

--- cory/src/App.jsx
+++ cory/src/App.jsx
@@ -1,15 +1,6 @@
-import { useEffect, useState } from "react";
 import Library from "./pages/LIbrary/Library";
 
 function App() {
-  const [message, setMessage] = useState("Loading...");
-
-  useEffect(() => {
-    fetch("http://localhost:8000")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message))
-      .catch((err) => setMessage("Error: " + err));
-  });
   return (
     Library()
   )
